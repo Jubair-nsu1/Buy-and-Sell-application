a601fb6dd6151e4347fc236bf58cf25fcf4280ca
jubair421 <a.k.m.ahmed@northsouth.edu>
2023-02-08 20:20:05 +0600

    update node modules

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -1,21 +1,20 @@
 import { FaTimes } from 'react-icons/fa'
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-const Product = ({title,price,rent,description,onDelete,onUpdate}) => {
+const Product = ({productID, title,price,rent,rentype,description,onDelete,onUpdate}) => {
     return (
         <div class="container mt-3">
-            <div class="card shadow p-2 mb-4 bg-body-tertiary rounded" style="width: 50rem; cursor: pointer" onClick={() => onUpdate(task.id)}>
+            <div class="card shadow p-2 mb-4 bg-body-tertiary rounded" style="width: 50rem; cursor: pointer" onClick={() => onUpdate(productID)}>
                 <div class="card-body">
                     <div>
-                        <h1>My First Bootstrap Page</h1>
-                        <p style="font-weight:bold">This part is inside a container class.</p>
-                        <p>The .container class provides a responsive fixed width container.</p>
-                        <p>Resize the browser window to see that the container width will change at different breakpoints.</p>
+                        <h1>{title}</h1>
+                        <p style="font-weight:bold">Price: {price}  |  Rent: {rent}/{rentype}</p>
+                        <p>{description}</p>
                     </div>
                     <div>
-                        <FaTimes
+                        <faTimes
                             style={{ color: 'red', cursor: 'pointer' }}
-                            onClick={() => onDelete(task.id)}
+                            onClick={() => onDelete(productID)}
                         />
                     </div>
                 </div>
